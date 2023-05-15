fea5aa235ad9bf70f3d47be8240dde9c21e31b77
bp5555b <chanon772@gmail.com>
2023-05-15 16:44:34 +0700

    update git push #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
 
 @Component({
+  imports: [HomeComponent, RouterModule],
+  standalone: true,
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'homes';
 }
